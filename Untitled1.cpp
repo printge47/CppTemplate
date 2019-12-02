2481c436ede2c8c0b1bd1fc36b7200a6e7bea033
Жанна Фролова <printge@yandex.ru>
2019-12-02 14:22:27 +0300

    Добавлен шаблон
    Шаблон предназначен для начала изучения языка С++.

---
+++ Untitled1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+int main()
+{
+	int S, y, x, i, N;
+	int maxX = 0;
+	int maxY = 0; 
+	cout<<"N =";
+	cin>>N;
+
+	for (i= 1; i<=N; i++)
+						{
+						cin>>x;
+						cin>>y;
+						if (abs(x)!=0 && abs(y)==0 && abs(x)>maxX)	maxX=abs(x);
+						if (abs(y)!=0 && abs(x)==0 && abs(y)>maxY)	maxY=abs(y);
+						}
+	if (maxX==0 && maxY== 0 )
+							cout <<"NO"<< endl;
+	else	
+		cout<<(maxX*maxY)/2<<endl;	
+
+}
+
+
